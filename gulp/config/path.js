8243b2304702ef29048b87308e9f7cdbc098dec8
egorsv1985 <egorvs2009@gmail.com>
2022-03-14 22:14:22 +0300

    change pug with html

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -19,7 +19,7 @@ export const path = {
 		images: `${srcFolder}/img/**/*.{jpg, jpeg, ico, png, gif, webp}`,
 		svg: `${srcFolder}/img/**/*.svg`,
 		scss: `${srcFolder}/scss/style.scss`,
-		html: `${srcFolder}/*.pug`,
+		html: `${srcFolder}/*.html`,
 		files: `${srcFolder}/files/**/*.*`,
 		svgicons: `${srcFolder}/svgicons/*.svg`
 	},
