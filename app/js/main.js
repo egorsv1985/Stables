2f479ca209d73d82be8487ced7b090a2d535d1da
AlexIstra1989 <alex888fcsm@gmail.com>
2022-03-16 23:19:32 +0300

    added event and offers blocks

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,25 @@
 $(function() {
-  $('.event__content-slider').slick ({
+  $('.event-slider').slick ({
    dots: true,
    arrows: false,
    infinite: true,
    slidesToShow: 1,
-   slidesToScroll: 1
+   slidesToScroll: 1,
+   responsive: [
+    {
+    breakpoint: 1600,
+    settings: "unslick"
+    },
+    {
+      breakpoint: 768,
+      setting:{
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    
+    }
+    }
+    ]
   });
 });
+
