5ca80a230d6608336c0ba8e3d59eeea6dbdb78cb
Vitalik9135eb2 <05vp94@mail.ru>
2022-03-17 20:12:49 +0300

    gallery

---
+++ src/js/app.js
@@ -0,0 +1,45 @@
+
+
+
+
+$(document).ready(function () {
+
+    $('.gallery__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // centerMode: true,
+        //               centerPadding: '0px',
+        dots: true
+    });
+
+
+    $('.gallery__slider-item').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+
+}); 
+
+
+
