5d6a67fc8af6027b2c9520c864ded852e5796667
Vitalik9135eb2 <05vp94@mail.ru>
2022-03-22 17:58:12 +0300

    page 3 fix

--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,6 @@
 $(document).ready(function () {
 
     $('.gallery__slider').slick({
-        // infinite: true,
-        // slidesToShow: 1,
-        // slidesToScroll: 1,
-        // centerMode: true,
-        //               centerPadding: '0px',
         dots: true
     });
 
@@ -20,12 +15,7 @@ $(document).ready(function () {
 		closeOnContentClick: false,
 		closeBtnInside: false,
 		mainClass: 'mfp-with-zoom mfp-img-mobile',
-		// image: {
-		// 	verticalFit: true,
-		// 	titleSrc: function(item) {
-		// 		return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-		// 	}
-		// },
+		
 		gallery: {
 			enabled: true
 		},
@@ -49,7 +39,7 @@ $(document).ready(function () {
 		 slidesToScroll: 1,
 		 responsive: [
 		  {
-		  breakpoint: 1600,
+		  breakpoint: 1900,
 		  settings: "unslick"
 		  },
 		  {
@@ -68,65 +58,21 @@ $(document).ready(function () {
 }); 
 
 
+const formBtn = document.querySelectorAll('.form__place');
 
-const ctx = document.getElementById('graph_activation');
-const myChart = new Chart(ctx, {
-	type: 'bar',
-	data: {
-		labels: ['Jun', 'Dec', 'Nov', 'Oct', 'Sep', 'Aug'],
-		datasets: [
-			/* Параметры первой таблицы для IOS*/
-			{
-				label: '',
-				data: [11, 11, 11, 11, 11, 11],
-				backgroundColor: [
-					'#0073FF',
-				],
-				borderColor: [
-					'#0073FF',
-				],
-				borderWidth: 1
-			},
-			/* Параметры первой таблицы для Android*/
-			{
-				label: '',
-				data: [7, 7, 7, 7, 7, 7],
-				backgroundColor: [
-					'#009E8B',
-				],
-				borderColor: [
-					'#009E8B',
-				],
-				borderWidth: 1,
-
-			},
-
-		],
-
-	},
-});
+formBtn.forEach((item) => {
+	item.addEventListener("click", (e) => {
+		e.preventDefault();
+		formBtn.forEach((item) => {
+			item.classList.remove('active')
+		})
+		
+		item.classList.add('active')
+	})
+})
+document.querySelector('.form__place').click()
 
 
-let ctx2 = document.getElementById('graph_retention');
-let chart = new Chart(ctx2, {
-	type: 'line',
-	data: {
-		labels: ['1m', '2m', '4m', '6m', '8m', '10m', '12m'],
-		datasets: [{ // График зелёного цвета
-				label: 'График 1',
-				backgroundColor: 'transparent',
-				borderColor: 'green',
-				data: [70, 60, 60, 50, 40, 30, 20],
-			},
-			{ // График синего цвета
-				label: 'График 2',
-				// backgroundColor: 'transparent',
-				borderColor: 'blue',
-				data: [90, 80, 80, 70, 60, 50, 40]
-			}
-		],
-	},
-});
 
 
 
