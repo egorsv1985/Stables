4161fd051f28e5bd06d28a3f58600de1e22e5245
egorsv1985 <egorvs2009@gmail.com>
2022-03-25 13:13:20 +0300

    fixed bags hover

--- src/js/app.js
+++ src/js/app.js
@@ -24,47 +24,44 @@ import Swiper, {
 const swiper = new Swiper();
 
 
-$(function() {
-    $('.event-slider').slick ({
-     dots: true,
-     arrows: false,
-     infinite: true,
-     slidesToShow: 1,
-     slidesToScroll: 1,
-     responsive: [
-      {
-      breakpoint: 19000,
-      settings: "unslick"
-      },
-      {
-        breakpoint: 768,
-        setting:{
-			settings: "slick",
-          speed: 300,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-      }
-      }
-      ]
-    });
-  });
+$(function () {
+	$('.event-slider').slick({
+		dots: true,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		responsive: [{
+				breakpoint: 19000,
+				settings: "unslick"
+			},
+			{
+				breakpoint: 768,
+				setting: {
+					settings: "slick",
+					speed: 300,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			}
+		]
+	});
+});
 
 
 
 
-  $(document).ready(function () {
+$(document).ready(function () {
 
-    $('.gallery__slider').slick({
-        infinite: true,
-        // slidesToShow: 1,
-        // slidesToScroll: 1,
-        // centerMode: true,
-        //               centerPadding: '0px',
-        dots: true
-    });
+	$('.gallery__slider').slick({
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		dots: true
+	});
 
 
-    $('.gallery__slider-item').magnificPopup({
+	$('.gallery__slider-item').magnificPopup({
 		delegate: 'a',
 		type: 'image',
 		closeOnContentClick: false,
@@ -82,11 +79,11 @@ $(function() {
 		zoom: {
 			enabled: true,
 			duration: 300, // don't foget to change the duration also in CSS
-			opener: function(element) {
+			opener: function (element) {
 				return element.find('img');
 			}
 		}
-		
+
 	});
 
 });
@@ -99,7 +96,7 @@ formBtn.forEach((item) => {
 		formBtn.forEach((item) => {
 			item.classList.remove('active')
 		})
-		
+
 		item.classList.add('active')
 	})
 })
@@ -178,5 +175,3 @@ export function menuClose() {
 }
 
 menuInit();
-
-
